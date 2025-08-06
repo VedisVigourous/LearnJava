67a8dd7cc2ba7d9bb31ac23ee5223aa203e2c260
Vedant <codegrounds666@gmail.com>
2025-08-07 00:35:57 +0530

    init commit

---
+++ src/AdvancedJava/AdvJava_Ch15_01_CollectionFramerwork.java
@@ -0,0 +1,9 @@
+package AdvancedJava;
+
+public class AdvJava_Ch15_01_CollectionFramerwork {
+    public static void main(String[] args) {
+        System.out.println();
+
+
+    }
+}
---
+++ src/Ch14_92_BasicJava_Full_RevisionClass.java
@@ -0,0 +1,6 @@
+public class Ch14_92_BasicJava_Full_RevisionClass {
+    public static void main(String[] args) {
+
+        // Starting from the Very Beginning - LET'S REVISE
+    }
+}
