12186870e30c68a198b2c594b2e1b8ad7f79e998
Vedant <codegrounds666@gmail.com>
2025-08-13 14:35:00 +0530

    init commit

--- src/AdvancedJava/AdvJava_Ch15_01_CollectionFramerwork.java
+++ src/AdvancedJava/AdvJava_Ch15_01_CollectionFramerwork.java
@@ -4,6 +4,29 @@ public class AdvJava_Ch15_01_CollectionFramerwork {
     public static void main(String[] args) {
         System.out.println();
 
+        // Collection Framework - It consist of different classes and Interfaces, which helps us manage our data in a better and efficient ways
 
+        /* Collection Framework vs Arrays -
+           Arrays are a great way but if we want to manipulate data it causes problems
+           Eg. If an database is made of certain limit and suddenly new entries are required then the task become challenging.
+               i.e. tasks like -
+               1. Inserting an element in between.
+               2. Deleting an element
+               3. Applying operation to manipulate data , creates a challenge.
+
+        ~~ To tackle all these problems we use - Collection Frameworks. */
+
+        // These Frameworks are accessible using Classes and even Frameworks. + Some Static Methods.
+
+        /* Commonly Used collections - Their uses
+           1. ArrayList - Data with variable size
+           2. Set - Distinct collection (No repeated element)
+           3. Stack - A LIFO data structure (LIFO - Last In , First Out - i.e. like plates , stacked on top of other that is last plate will be the first to be taken)
+           4. HashMap - For storing key - value pairs (for pairs - Example - [Social Media account 🔗 Password] ). */
+
+        // Collection Class is present in java.util package and provides methods for a. Sorting
+        //                                                                           b. Searching etc.
+
+        // Syntax - java.util.collection
     }
 }
