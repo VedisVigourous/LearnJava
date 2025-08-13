4373810fa6e3620b612cd464ba0a29d431989223
Vedant <codegrounds666@gmail.com>
2025-08-13 15:45:50 +0530

    init commit

---
+++ src/AdvancedJava/AdvJava_Ch15_02_CreatingCollections.java
@@ -0,0 +1,20 @@
+package AdvancedJava;
+
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.Stack;
+import java.util.HashMap;
+import java.util.TreeSet;
+
+public class AdvJava_Ch15_02_CreatingCollections {
+    public static void main(String[] args) {
+        System.out.println();
+
+        // To call collections - we need to Import them
+        // 1. ArrayList - Line 3
+        // 2. Set - Line 4
+        // 3. Stack - Line 5
+        // 4. HashMap - Line 6
+        // 5. TreeSet - Line 7 and so on...
+    }
+}
