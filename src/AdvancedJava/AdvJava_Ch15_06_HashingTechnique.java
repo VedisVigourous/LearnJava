78d30976350ef66db54c36d45933f814d77a80b4
Vedant <codegrounds666@gmail.com>
2025-08-20 18:29:44 +0530

    init commit

--- src/AdvancedJava/AdvJava_Ch15_06_HashingTechnique.java
+++ src/AdvancedJava/AdvJava_Ch15_06_HashingTechnique.java
@@ -3,9 +3,9 @@ package AdvancedJava;
 public class AdvJava_Ch15_06_HashingTechnique {
 
     /* Hashing Technique -
-     * It bounds a KeyValue to an Index (KeyValue ----> Discrete Index)
+     * It bounds a KeyValue to an Index (KeyValue ----> Discrete Index) */
 
-     ~ Internal Working -
+    /* ~ Internal Working -
        * used a Fn = f(x) = x % (number of Indices)
          -> x here is the value that we want to insert in our HashTable
        Example:
@@ -19,7 +19,11 @@ public class AdvJava_Ch15_06_HashingTechnique {
         2. f(25) = 25 % 10 = 5 (5th index alloted)
         2. f(13) = 13 % 10 = 3 (3rd index alloted)
 
-        * i.e. this technique is very fast in index alloting */
+        * i.e. this technique is very fast in index alloting
+
+        *** Known Problems ***
+        1. HashCollision - When two numbers are to be alloted on an same index ex. 11 & 21 both will get 1st index
+           1.1 Solution [Open Addressing- Increasing indexes i.e. Empty spaces are kept in HashTable so that the other number be alloted to nearest empty index. */
     public static void main(String[] args) {
 
     }
