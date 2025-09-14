0e8d42c94c69675fa8a3151a8461f5b8a0ddb63c
Vedant <codegrounds666@gmail.com>
2025-09-15 02:02:48 +0530

    pending code...

---
+++ src/AdvancedJava/AdvJava_Ch17_16_LibraryManagement.java
@@ -0,0 +1,16 @@
+package AdvancedJava;
+
+public class AdvJava_Ch17_16_LibraryManagement {
+
+    /* Guidelines of Project - Library Management System :
+
+       * Must be capable of Issuing Books.
+       * Info must be like 1. Bookname
+                           2. Author
+                           3. Issued to
+                           4. Issue Date
+
+       * Must be Iterative until user don't want to access the LMS again
+       * User must be able to add books and return issue books (better if created to spaces consumer and owner)
+       * Books issuing must be only available to card-holders     */
+}
