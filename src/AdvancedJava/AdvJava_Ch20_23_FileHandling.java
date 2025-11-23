57f3b2177871d42c1cae3fe529e5f57c9279b16a
VedisVigourous <vadanta592007@hotmail.com>
2025-11-23 23:41:06 +0530

    File Handling

--- src/AdvancedJava/AdvJava_Ch20_23_FileHandling.java
+++ src/AdvancedJava/AdvJava_Ch20_23_FileHandling.java
@@ -1,8 +1,10 @@
 package AdvancedJava;
 import javax.imageio.IIOException;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class AdvJava_Ch20_23_FileHandling {
     public static void main(String[] args) {
@@ -19,20 +21,17 @@ public class AdvJava_Ch20_23_FileHandling {
          */
 
         /*
-
-
         File creation = new File("C:\\Users\\Vedant\\Documents\\PlayGrounds\\MyJava\\src\\AdvancedJava\\HandledFile\\AdvJava_01_FileHandling.txt");
         try {
             //noinspection ResultOfMethodCallIgnored
-            creation.createNewFile();\
+            creation.createNewFile();
             System.out.println("Successfully Created a file !");
         }
         catch (IOException e){
             System.out.println("Unable to Create this file ! -- " + e);
         }
 
-
-         */ // Commented in order to Handle the created file further.
+        */  // Commented in order to Handle the created file further [Used later too]
 
         // New Method : Writing to a file
         /* We use FileWriter Class:
@@ -42,15 +41,56 @@ public class AdvJava_Ch20_23_FileHandling {
 
            * Note Surround everything under : try catch to prevent IOException */
 
+        /*
         try {
             FileWriter fileWriter = new FileWriter("C:\\Users\\Vedant\\Documents\\PlayGrounds\\MyJava\\src\\AdvancedJava\\HandledFile\\AdvJava_01_FileHandling.txt");
             fileWriter.write("* This is my First Handled file using Java \n  ~ Thank You");
             fileWriter.close();
-            System.out.println("Succesfully Written the file !");
+            System.out.println("Succesfully Written the file !"); // Notes: Resets all the content of the file with the specified content.
         }
         catch (IOException e){
             System.out.println(e);
         }
+        */
 
+        /* Reading a file:
+           Using File class and Scanner Class -- Handle FileNotFound Exception
+
+           Syntax: Object creation with Path and -- Scanner <name> = new Scanner(Object_Name);
+           As scanner class reads the content we need to run a loop to print the content and close the file */
+
+        /*
+        try {
+            File readingFile = new File("C:\\Users\\Vedant\\Documents\\PlayGrounds\\MyJava\\src\\AdvancedJava\\HandledFile\\AdvJava_01_FileHandling.txt");
+            Scanner reading = new Scanner(readingFile);
+
+            while (reading.hasNextLine()){ // Runs till the file has string
+                String line = reading.nextLine(); // allocates the string in this variable
+                System.out.println(line); // Prints the variable
+            }
+
+            reading.close();
+        }
+        catch (FileNotFoundException e){
+            System.out.println(e);
+        }
+        */
+
+        /* Deleting a file:
+           * Creating an object with path
+           * Using .delete() method
+           * Writing in if-else - for a dynamic program  */
+
+        /*
+            File delFile = new File("C:\\Users\\Vedant\\Documents\\PlayGrounds\\MyJava\\src\\AdvancedJava\\HandledFile\\AdvJava_01_FileHandling.txt");
+            if (delFile.delete()){
+                System.out.println(delFile.getName() + " has been Deleted!"); // getName() - prints the name of the file
+            }
+            else {
+                System.out.println("Unable to Delete !");
+            }
+        */
     }
 }
+
+// Note: Other methods on :: @https://docs.oracle.com/en/java/javase/24/docs/api/java.base/java/io/File.html#method-summary
