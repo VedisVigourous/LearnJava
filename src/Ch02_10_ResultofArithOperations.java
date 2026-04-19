867d5108d9311544888c294dd762490707a3fb19
VedisVigourous <vadanta592007@hotmail.com>
2026-04-19 22:20:33 +0530

    Type Casting Explained

--- src/Ch02_10_ResultofArithOperations.java
+++ src/Ch02_10_ResultofArithOperations.java
@@ -15,5 +15,17 @@ public class Ch02_10_ResultofArithOperations {
         float a = 3.598f;
         double b = 8.47; // By default, double
         System.out.println(a + b); //Will be double
+        // Printing the type of result of a+b
+        System.out.println(((Object)(a+b)).getClass().getSimpleName()); // Will be double
+
+
+        /* TypeCasting */
+        int num = (int) 39479823.342f;
+        System.out.println(num);
+
+        /* Explanation: Even though the number provided is a float, we are typecasting it to int. So, the result will be an integer. */
+
+        /* NOTE:::: That is: TypeCasting happens only from smaller to bigger datatypes.
+           Ex. If input is for int and output is for float, then the result will be 'float'. But if input is for float and output is for int, then the result will be int.  */
     }
 }
